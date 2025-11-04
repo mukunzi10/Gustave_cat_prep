35719c83c345ce6382d46cc7a5b354517a21856c
mukunzi10 <mukunzigustavus@gmail.com>
2025-11-04 20:13:56 +0200

    hello

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration for XAMPP
 $host = 'localhost';
-$dbname = '24pr14238_shareride_db';
+$dbname = 'db_24rp14238_shareride';
 $username = 'root';
 $password = '';  // XAMPP default has no password
 
@@ -12,7 +12,6 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 // Set charset to utf8
 $conn->set_charset("utf8");
 ?>
